7c0200b84b5774adc6247a06a996bfecbcb8894e
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2022-08-31 15:37:11 +0530

    assignment done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
+import { Button } from "./Button"
 
 function App() {
+  const [isShow, setIsShow] = useState(false);
+  const handleClick = () => {
+    setIsShow(!isShow);
+  } 
+
   return (
     <div id="main">
       // Do not alter the main div
+      <Button onToggle={handleClick} />
+      {isShow && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+
     </div>
   );
 }
